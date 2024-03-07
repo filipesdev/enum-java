a4a8fe44d3c87fcaebc5af2676674a314593835d
Filipemtb <filipera3@gmail.com>
2024-03-07 13:58:49 -0300

    create: test application

---
+++ exercicioEnumeracao/src/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import entities.Order;
+import enums.OrderStatus;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+    }
+}
