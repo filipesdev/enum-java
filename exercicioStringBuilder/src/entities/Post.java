91491902668cc8e9f5afc18ab2e0e1a58b3836a9
Filipemtb <filipera3@gmail.com>
2024-03-11 15:14:16 -0300

    create: add toString using StringBuilder
    Aprendendo como utilizar a classe StringBuilder montando uma string grande a partir de outras string menores

--- exercicioStringBuilder/src/entities/Post.java
+++ exercicioStringBuilder/src/entities/Post.java
@@ -1,10 +1,13 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     private Date moment;
     private String title;
     private String content;
@@ -56,5 +59,23 @@ public class Post {
 
     public void removeComment(Comment comment) {
         comments.remove(comment);
+    }
+
+
+    // criando o toString utilizando o StringBuilder
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(title + "\n");
+        sb.append(likes);
+        sb.append(" Likes - ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append(content + "\n");
+        sb.append("Comments \n");
+        for (Comment comment : comments) {
+            sb.append(comment.getText() + "\n");
+        }
+        return sb.toString();
     }
 }
