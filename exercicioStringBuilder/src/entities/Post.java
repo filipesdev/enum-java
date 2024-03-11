7786dfd0a2ddb2185b7f1d3c1c3717b328e107bb
Filipemtb <filipera3@gmail.com>
2024-03-11 14:52:11 -0300

    create: add post class

---
+++ exercicioStringBuilder/src/entities/Post.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+    private Date moment;
+    private String title;
+    private String content;
+    private Integer likes;
+
+    // quando for uma lista, não fazer o set. fazer o metodo add e remove.
+    private List<Comment> comments = new ArrayList<>();
+
+    public Post() {}
+
+    public Post(Date moment, String title, String content, Integer likes) {
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void removeComment(Comment comment) {
+        comments.remove(comment);
+    }
+}
